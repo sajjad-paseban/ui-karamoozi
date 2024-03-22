f71705c7d815b69413680ba4eecb9b45e346a1a8
iamsajad <s.pr98@yahoo.com>
2024-03-22 13:25:16 +0330

    header partial completed

---
+++ src/partials/HeaderPartial.vue
@@ -0,0 +1,56 @@
+<template>
+    <header>
+        <div class="left-side">
+            <Button 
+                :isLink="true"
+                group="alpha"
+                btnClass="btn btn-light"
+                path="/login"
+                title="ورود به پرتال"
+            >
+            <FeLogIn />            
+        </Button>            
+        </div>
+        <nav>
+            <header-menu-partial />
+            <logo 
+                title="دانشگاه فنی و حرفه ای مشهد"
+                :isLight="true" 
+                />
+   
+        </nav>
+    </header>
+</template>
+
+<script lang="ts">
+import Logo from "@/components/Logo.vue";
+import { defineComponent } from "vue";
+import HeaderMenuPartial from "./HeaderMenuPartial.vue";
+import Button from "@/components/Button.vue";
+import { FeLogIn } from "@kalimahapps/vue-icons";
+export default defineComponent({
+  components: { Logo, HeaderMenuPartial, Button, FeLogIn },
+    name: 'header-partial',
+})
+</script>
+
+<style lang="scss" scoped>
+    header{
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+        background-color: #263238;
+        nav{
+            display: flex;
+            align-items: center;
+        }
+
+        .left-side{
+            display: flex;
+            align-items: center;
+            margin: 0 10px;
+            position: relative;
+            bottom: 1px;
+        }
+    }
+</style>
