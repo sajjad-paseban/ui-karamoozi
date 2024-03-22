910e109583f88f661c742b3e6082ed6150eca862
iamsajad <s.pr98@yahoo.com>
2024-03-22 13:21:45 +0330

    button component added

---
+++ src/components/Button.vue
@@ -0,0 +1,41 @@
+<template>
+    <div :class="group">
+        <router-link :class="btnClass" v-if="isLink" :to="path ? path : ''">
+            {{ title }}
+            <span>
+                <slot />
+            </span>
+        </router-link>
+        <button :class="btnClass" v-if="!isLink">
+            {{ title }}
+            <span>
+                <slot />
+            </span>
+        </button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'button-component',
+    props: ['group', 'btnClass', 'isLink', 'path', 'title']
+})
+</script>
+
+<style lang="scss" scoped>
+    button, a{
+        margin: 0 !important;
+        padding: 5px 14px !important;
+        display: inline-flex !important;
+        font-size: 12px !important;
+        font-family: 'yekan' !important;
+        
+        span{
+            position: relative;
+            top: 3px;
+            font-size: 12px;
+            left: 3px;
+        }
+    }
+</style>
