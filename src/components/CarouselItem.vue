d3a463cc5d9fc908cbea294aa1d81a5b1a511c50
iamsajad <s.pr98@yahoo.com>
2024-03-22 13:24:05 +0330

    carousel item component added

---
+++ src/components/CarouselItem.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="item">
+        <img :src="imgPath ? imgPath : 'https://picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg'" alt="">
+        <div v-if="caption" class="cover"></div>
+        <div v-if="caption" class="caption">
+            <h1>
+                {{ caption }}
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'carousel-item-component',
+    props: ['imgPath', 'caption']
+})
+</script>
+
+<style lang="scss" scoped>
+    .item{
+        position: relative;
+        height: 550px;
+
+        img{
+            width: 100%;
+            height: 550px;
+            object-fit: cover;
+            object-position: center center;
+        }
+        .cover{
+            position: absolute;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            background-color: rgba($color: #000000, $alpha: 0.5);
+        }
+        .caption{
+            position: absolute;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            z-index: 5;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            user-select: none;
+            h1{
+                font-family: 'KOODAK';
+                color: #fff;
+            }
+        }
+    }
+</style>
