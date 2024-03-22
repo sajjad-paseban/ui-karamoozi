275a2e7c6b6aa1b840fdc569083414a00c8c1337
iamsajad <s.pr98@yahoo.com>
2024-03-22 13:25:57 +0330

    footer partial added

---
+++ src/partials/FooterPartial.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer>
+        footer part
+    </footer>
+</template>
+
+<script lang="ts">
+import Vue, { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'footer-partial'
+})
+</script>
