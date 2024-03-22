20a7222c05febe61224e2246922c7d718fa0c823
iamsajad <s.pr98@yahoo.com>
2024-03-22 13:26:44 +0330

    views added

---
+++ src/views/landing/ContactUsView.vue
@@ -0,0 +1,3 @@
+<template>
+    i am contact us page
+</template>
---
+++ src/views/landing/IndexView.vue
@@ -0,0 +1,12 @@
+<template>
+    <header-banner-partial />
+</template>
+
+<script lang="ts">
+import HeaderBannerPartial from '@/partials/HeaderBannerPartial.vue'
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'landing-index-view',
+    components: { HeaderBannerPartial },
+})
+</script>
