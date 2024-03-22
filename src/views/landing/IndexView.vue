df9e336896e1791a67be6c1df423217ea5e8c91b
iamsajad <s.pr98@yahoo.com>
2024-03-22 17:24:18 +0330

    NewsContentPartial added in file

--- src/views/landing/IndexView.vue
+++ src/views/landing/IndexView.vue
@@ -1,12 +1,15 @@
 <template>
     <header-banner-partial />
+    <news-content-partial />
+
 </template>
 
 <script lang="ts">
 import HeaderBannerPartial from '@/partials/HeaderBannerPartial.vue'
+import NewsContentPartial from '@/partials/NewsContentPartial.vue'
 import { defineComponent } from 'vue'
 export default defineComponent({
     name: 'landing-index-view',
-    components: { HeaderBannerPartial },
+    components: { HeaderBannerPartial, NewsContentPartial },
 })
 </script>
