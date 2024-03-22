c22f6edff20129f73ac869212b830e5fe851114a
iamsajad <s.pr98@yahoo.com>
2024-03-22 13:24:32 +0330

    landing layout added

---
+++ src/layout/LandingLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <header-partial />
+    <router-view />
+    <footer-partial />
+</template>
+<script lang="ts">
+import HeaderPartial from '@/partials/HeaderPartial.vue'
+import FooterPartial from '@/partials/FooterPartial.vue'
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'landing-layout',
+    components: {
+        HeaderPartial,
+        FooterPartial
+    }
+})
+</script>
