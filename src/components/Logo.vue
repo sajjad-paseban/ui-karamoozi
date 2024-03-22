ff9c1ad36d2b783116b904f34c315678355a909e
iamsajad <s.pr98@yahoo.com>
2024-03-22 13:24:16 +0330

    logo component added

---
+++ src/components/Logo.vue
@@ -0,0 +1,30 @@
+<template>
+    <span v-if="title">{{ title }}</span>
+    <img :class="{light: isLight}" :src="path ? path : 'https://upload.wikimedia.org/wikipedia/commons/d/de/TVU_logo.svg'" alt="">
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'logo-component',
+    props: ["isLight","title","path"]
+})
+</script>
+
+<style lang="scss" scoped>
+    img{
+        padding: 10px;
+        width: 40px;
+        height: auto;
+        &.light{
+            filter: brightness(0) invert(1);
+        }
+    }
+    span{
+        font-family: 'yekan';
+        font-size: 12px;
+        color: #fff;
+        opacity: 0.95;
+        user-select: none;        
+    }
+</style>
