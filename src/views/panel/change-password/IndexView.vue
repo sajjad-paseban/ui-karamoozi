46fa71208ef1c3cea8b4e422a4801be41f391a58
iamsajad <s.pr98@yahoo.com>
2024-05-31 16:36:57 +0330

    vazir font added

---
+++ src/views/panel/change-password/IndexView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="row justify-content-end p-2">
+        <div class="col-auto">
+            <h1 class="text-secondary">
+                تغییر کلمه عبور
+            </h1>
+        </div>
+    </div>
+</template>
