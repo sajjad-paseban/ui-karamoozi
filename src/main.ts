6ba0d9409d03771b3d6cbb6d1018812979945e1b
iamsajad <s.pr98@yahoo.com>
2024-05-31 17:42:07 +0330

    vuetify added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,25 @@ import App from './App.vue'
 import router from './router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
+// vuetify config
+// import 'vuetify/styles'
+// import { createVuetify } from 'vuetify'
+// import { fa } from 'vuetify/locale'
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
+// const vuetify = createVuetify({
+//     locale:{
+//         locale: 'fa',
+//         fallback: 'fa',
+//         messages: { fa },
+//         rtl: {fa: true}
+//     },
+//     // theme: true,
+//     components,
+//     directives,
+// })
+// vuetify config
+
 import 'bootstrap/dist/css/bootstrap.css'
 import '@/assets/scss/global.scss'
 import 'primeicons/primeicons.css'
